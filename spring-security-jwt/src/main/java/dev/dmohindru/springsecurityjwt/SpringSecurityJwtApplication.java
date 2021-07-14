705aa44327f98d21a0eee503b1dc171cf7180a6b
Dhruv Mohindru <dmohindru@gmail.com>
2021-07-14 21:10:10 +1000

    Updated index.txt with new project details

--- spring-security-jwt/src/main/java/dev/dmohindru/springsecurityjwt/SpringSecurityJwtApplication.java
+++ spring-security-jwt/src/main/java/dev/dmohindru/springsecurityjwt/SpringSecurityJwtApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringSecurityJwtApplication {
+	// Github: https://github.com/koushikkothagal/spring-security-jwt
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityJwtApplication.class, args);
