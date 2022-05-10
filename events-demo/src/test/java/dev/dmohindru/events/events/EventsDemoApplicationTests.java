477f19447190d22802ff98c89e9e7f526003b13e
Dhruv Mohindru <dmohindru@gmail.com>
2022-05-11 08:25:08 +1000

    events demo experiment

---
+++ events-demo/src/test/java/dev/dmohindru/events/events/EventsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dmohindru.events.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
