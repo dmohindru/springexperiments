9ba40c683c15dcf960f073e7949593eb645ec565
Dhruv Mohindru <dmohindru@gmail.com>
2022-06-08 15:04:24 +1000

    First commit for feature flag demo project

---
+++ feature-flag-demo/src/test/java/dev/dmohindru/featureflagdemo/FeatureFlagDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dmohindru.featureflagdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureFlagDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
