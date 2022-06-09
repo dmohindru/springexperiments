cf23f6414349a7ba73cee7397d182568aa193808
Dhruv Mohindru <dmohindru@gmail.com>
2022-06-09 14:10:21 +1000

    Updated connection string

--- mongo-client-demo/src/main/java/dev/dmohindru/mongoclientdemo/MongoDBConnectionDemo.java
+++ mongo-client-demo/src/main/java/dev/dmohindru/mongoclientdemo/MongoDBConnectionDemo.java
@@ -14,7 +14,7 @@ public class MongoDBConnectionDemo {
         //String CONNECTION_STRING_OLD_DRIVER = "mongodb://pp-staging:2hucZFecf7dDcMCl@prodmaindb-shard-00-00.9nyog.mongodb.net:27017,prodmaindb-shard-00-01.9nyog.mongodb.net:27017,prodmaindb-shard-00-02.9nyog.mongodb.net:27017/?ssl=true&replicaSet=atlas-x37tpa-shard-0&authSource=admin&retryWrites=true&w=majority";
 
         String CONNECTION_STRING_NEW_DRIVER =
-                "mongodb+srv://pp-staging:2hucZFecf7dDcMCl@pp-mgd-staging.y1dgp.mongodb.net/ppMgdStagingDB";
+                "you mongo db secret: mongodb + srv connection string";
         ConnectionString connectionString = new ConnectionString(CONNECTION_STRING_NEW_DRIVER);
 
 
