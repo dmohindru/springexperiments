75642fbc674b6da9cf30739f1d51142d06ef8ada
Dhruv Mohindru <dmohindru@gmail.com>
2022-06-09 14:17:19 +1000

    Few typos

--- mongo-client-demo/src/main/java/dev/dmohindru/mongoclientdemo/MongoDBConnectionDemo.java
+++ mongo-client-demo/src/main/java/dev/dmohindru/mongoclientdemo/MongoDBConnectionDemo.java
@@ -11,7 +11,7 @@ import org.bson.Document;
 public class MongoDBConnectionDemo {
     public static void main(String[] args) {
 
-        //String CONNECTION_STRING_OLD_DRIVER = "mongodb://pp-staging:2hucZFecf7dDcMCl@prodmaindb-shard-00-00.9nyog.mongodb.net:27017,prodmaindb-shard-00-01.9nyog.mongodb.net:27017,prodmaindb-shard-00-02.9nyog.mongodb.net:27017/?ssl=true&replicaSet=atlas-x37tpa-shard-0&authSource=admin&retryWrites=true&w=majority";
+        //String CONNECTION_STRING_OLD_DRIVER = "old connection string format";
 
         String CONNECTION_STRING_NEW_DRIVER =
                 "you mongo db secret: mongodb + srv connection string";
