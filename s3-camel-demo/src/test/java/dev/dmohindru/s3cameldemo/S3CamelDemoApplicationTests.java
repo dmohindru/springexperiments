537a00471c9e767a1740724a01cddfa2d5a05291
Dhruv Mohindru <dmohindru@gmail.com>
2022-08-23 13:55:14 +1000

    Started new project

---
+++ s3-camel-demo/src/test/java/dev/dmohindru/s3cameldemo/S3CamelDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dmohindru.s3cameldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3CamelDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
