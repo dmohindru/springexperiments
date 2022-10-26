1a6452cfc67b7ba031403aaeb036f3f4a68760a2
Dhruv Mohindru <dmohindru@gmail.com>
2022-10-27 06:35:26 +1100

    Completed an example of complete kafka real world example project

---
+++ springboot-kafka-real-world-project/kafka-consumer-database/src/main/java/dev/dmohindru/springboot/repository/WikimediaDataRepository.java
@@ -0,0 +1,7 @@
+package dev.dmohindru.springboot.repository;
+
+import dev.dmohindru.springboot.entity.WikimediaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WikimediaDataRepository extends JpaRepository<WikimediaData, Long> {
+}
