66852a4c7315e56077a3406db616e76700ea3161
Dhruv Mohindru <dmohindru@gmail.com>
2022-11-14 12:17:47 +1100

    Have working example of how to serialize and deserialize spatial data to postgis database

--- springboot-postgres/src/main/java/dev/dmohindru/springbootpostgres/service/CityServiceImpl.java
+++ springboot-postgres/src/main/java/dev/dmohindru/springbootpostgres/service/CityServiceImpl.java
@@ -3,10 +3,15 @@ package dev.dmohindru.springbootpostgres.service;
 import dev.dmohindru.springbootpostgres.dao.CityDao;
 import dev.dmohindru.springbootpostgres.dto.CityDto;
 import dev.dmohindru.springbootpostgres.entity.City;
+import dev.dmohindru.springbootpostgres.exceptions.DuplicateException;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +19,8 @@ import org.springframework.stereotype.Service;
 public class CityServiceImpl implements CityService {
 
     private final CityDao cityDao;
+
+    private GeometryFactory geoFactory = new GeometryFactory();
 
     @Override
     public CityDto getCityById(String id) {
@@ -36,6 +43,41 @@ public class CityServiceImpl implements CityService {
     // TODO: Wrtie logic to create City and City.Point user GeometryFactory method
     @Override
     public CityDto saveCity(CityDto cityDto) {
-        return null;
+
+        // Throw exception if city is present with same id
+        City foundCity = cityDao.findCityById(cityDto.getId());
+        if (foundCity != null) {
+            throw new DuplicateException(String.format("City with Id %s already present", cityDto.getId()));
+        }
+
+        // Create city object
+        City city = City
+                .builder()
+                .id(cityDto.getId())
+                .altitude(cityDto.getElevation())
+                .population2010(cityDto.getPopulation())
+                .state(cityDto.getState())
+                .point(geoFactory.createPoint(
+                        new Coordinate(cityDto.getPosition().getLat(),
+                                cityDto.getPosition().getLng()))
+                )
+                .build();
+
+        City savedCity = cityDao.saveCity(city);
+
+        CityDto saveCityDto = CityDto.builder()
+                .id(savedCity.getId())
+                .elevation(savedCity.getAltitude())
+                .population(savedCity.getPopulation2010())
+                .state(savedCity.getState())
+                .position(CityDto.Position.builder()
+                        .lat(savedCity.getPoint().getX())
+                        .lng(savedCity.getPoint().getY())
+                        .build()
+
+                )
+                .build();
+
+        return saveCityDto;
     }
 }
