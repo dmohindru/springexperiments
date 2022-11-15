67d030e9c463d9eeb2a64b4b5e266ab3afa6c08c
Dhruv Mohindru <dmohindru@gmail.com>
2022-11-15 13:55:59 +1100

    Initial commit for feature file gcp_storage_example

---
+++ springboot-google-cloud-storage-example/src/main/java/dev/dmohindru/gcp/GoogleCloudStorageExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.dmohindru.gcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCloudStorageExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleCloudStorageExampleApplication.class, args);
+	}
+
+}
---
+++ springboot-google-cloud-storage-example/src/test/java/dev/dmohindru/gcp/GoogleCloudStorageExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dmohindru.gcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleCloudStorageExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
